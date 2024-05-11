e7755839af8c0873fd383fe60c3119340efc98e9
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-12 02:18:19 +0500

    creating the header of Home page

---
+++ front-end/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favotire food here</h2>
+            <p>Discover an array of flavors on our menu! From savory delights to sweet temptations, we offer a diverse selection to satisfy every craving. Explore and indulge in our culinary offerings for a delightful dining experience.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
