a04407ab168becdecdf8f5da77aaf8ca0d9e834c
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-12 02:31:41 +0500

    addin g the explore menu component in Home page

---
+++ front-end/src/components/ExploreMenu/ExploreMenu.tsx
@@ -0,0 +1,23 @@
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id='explore'>
+        <h1>Explore our Menu</h1>
+        <p className='explore-menu-text'>Choose from our menu featuring delectable array of food dishes. Our abbission is to satisfy your CRAVINGS!</p>
+        <div className="explore-menu-list">
+            {menu_list.map((item, index)=>{
+                return(
+                    <div className="explore-menu-list-item" key={index}>
+                        <img src={item.menu_image} alt=''/>
+                        <p>{item.menu_name}</p>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default ExploreMenu
