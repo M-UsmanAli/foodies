2aa7683f0fbb84e0c981fec31c8efc9038c36db4
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-14 22:14:02 +0500

    adding the card item functionality logically into the code!

--- front-end/src/context/StoredContext.tsx
+++ front-end/src/context/StoredContext.tsx
@@ -1,15 +1,38 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoredContext = createContext<any>(null)
 
-const StoredContextProvider=(props:any)=>{
+interface CartItems {
+    [key: string]: number; // Assuming itemId is a string, and the value is the quantity
+}
+const StoredContextProvider = (props: any) => {
+
+    const [cartItems, setCartItems] = useState<CartItems>({})
+
+    const addToCart = (itemId: any) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
+        }
+        else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        }
+    }
+
+    const removeFromCart = (itemId: any) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+
 
 
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
-    return(
+    return (
         <StoredContext.Provider value={contextValue}>
             {props.children}
         </StoredContext.Provider>
