5722d0cb652c6e6808a60c900966b3ba1c089789
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-14 20:17:32 +0500

    Made the random changes in the heading of Header

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2>Order your favotire food here</h2>
+            <h2>Order Your Favorite Food Here!</h2>
             <p>Discover an array of flavors on our menu! From savory delights to sweet temptations, we offer a diverse selection to satisfy every craving. Explore and indulge in our culinary offerings for a delightful dining experience.</p>
             <button>View Menu</button>
         </div>
