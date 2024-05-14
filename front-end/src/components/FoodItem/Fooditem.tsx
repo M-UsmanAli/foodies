a0734d5924da0ca48befa38081de73e7f9d5836c
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-14 22:50:46 +0500

    integration of cart with the food item component

--- front-end/src/components/FoodItem/Fooditem.tsx
+++ front-end/src/components/FoodItem/Fooditem.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { assets } from '../../assets/assets'
 import './Fooditem.css'
+import { StoredContext } from '../../context/StoredContext'
 
 interface Props {
     id: number
@@ -12,18 +13,19 @@ interface Props {
 const Fooditem = ({ id, name, price, image, description }: Props) => {
 
     const [itemCount, setItemCount] = useState(0)
+    const {cartItems, addToCart, removeFromCart}= useContext(StoredContext)
 
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
                 <img src={image} alt="" className="food-item-image" />
                 {
-                    !itemCount?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt=''/>
+                    !cartItems[id]?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt=''/>
                     :
                     <div className='food-item-counter'>
-                        <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt=''/>
-                        <p>{itemCount}</p>
-                        <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt=''/>
+                        <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt=''/>
+                        <p>{cartItems[id]}</p>
+                        <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt=''/>
                     </div>
                 }
             </div>
