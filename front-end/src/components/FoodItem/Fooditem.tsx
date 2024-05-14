8ae50acc4eae02fbf17559a453ac3b649de35ff8
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-14 21:18:46 +0500

    adding the functionaity of food item count

--- front-end/src/components/FoodItem/Fooditem.tsx
+++ front-end/src/components/FoodItem/Fooditem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { assets } from '../../assets/assets'
 import './Fooditem.css'
 
@@ -9,15 +10,27 @@ interface Props {
     description: string
 }
 const Fooditem = ({ id, name, price, image, description }: Props) => {
+
+    const [itemCount, setItemCount] = useState(0)
+
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
                 <img src={image} alt="" className="food-item-image" />
+                {
+                    !itemCount?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt=''/>
+                    :
+                    <div className='food-item-counter'>
+                        <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt=''/>
+                        <p>{itemCount}</p>
+                        <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt=''/>
+                    </div>
+                }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
                     <p>{name}</p>
-                    <img src={assets.rating_starts} alt=''/>
+                    <img src={assets.rating_starts} alt='' />
                 </div>
                 <p className="food-item-description">
                     {description}
