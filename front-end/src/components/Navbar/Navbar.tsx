8179d141caee38e03eec00655a5295687bbf5168
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-16 19:58:35 +0500

    created & designed the cart

--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = ({setShowLogin}:Props) => {
     const [menu, setMenu] = useState("home")
     return (
         <div className='navbar'>
-            <img src={assets.logo} alt='' className='logo' />
+            <Link to='/'><img src={assets.logo} alt='' className='logo' /></Link>
             
             <ul className="navbar-menu">
                 <Link to='/' onClick={() => setMenu('home')} className={menu === 'home' ? 'active' : ""}>Home</Link>
@@ -23,7 +23,7 @@ const Navbar = ({setShowLogin}:Props) => {
             <div className="navbar-right">
                 <img src={assets.search_icon} alt='' />
                 <div className="navbar-search-icon">
-                    <img src={assets.basket_icon} alt='' />
+                    <Link to='/cart'><img src={assets.basket_icon} alt='' /></Link>
                     <div className="dot"></div>
                 </div>
                 <button onClick={()=>setShowLogin(true)}>Sign in</button>
--- front-end/src/pages/Cart/Cart.tsx
+++ front-end/src/pages/Cart/Cart.tsx
@@ -1,6 +1,72 @@
+import { useContext } from 'react'
+import { StoredContext } from '../../context/StoredContext'
 import './Cart.css'
 export const Cart = () => {
+  const { cartItems, food_list, removeFromCart } = useContext(StoredContext)
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item: any, index: any) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className='cart-items-title cart-items-item'>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>X</p>
+                </div>
+                <hr />
+              </div>
+            )
+          }
+        })}
+      </div>
+      <div className='cart-bottom'>
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Sub Total</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+          </div>
+          <button>Proceed To CheckOut</button>
+        </div>
+
+        <div className="cart-promocode">
+          <div>
+            <b>If you have a promocode, Enter it here</b>
+            <div className='cart-promocode-input'>
+              <input type="text"  placeholder='promo code'/>
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
